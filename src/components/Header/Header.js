fd33ca2cdf85fabc62bd08d79c086cd13f44e4fe
Jason Ephraim <jason.ephraim.novatex@gmail.com>
2021-01-05 11:42:53 -0600

    Add scss and header/body components
    add bootstrap.min.css and syles folders
    
    fix issues with import and added menu
    
    connect bootstrap and add style to menu
    
    remove duplicate styling from app
    
    add last line to js files and other requested fix

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,66 @@
 import React from "react";
 
-function Header() {
+const Header = () => {
+    
+  
     return (
-        <header>
-            <h1>Our React Starter</h1>
-        </header>
+        <nav className="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+          <div
+            className="navbar-brand"
+          >
+            react-o-shop
+          </div>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarsExampleDefault"
+            aria-controls="navbarsExampleDefault"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+  
+          <div className="collapse navbar-collapse" id="navbarsExampleDefault">
+            <ul className="navbar-nav mr-auto">
+              <li className="nav-item active">
+                <div
+                  className="nav-link"
+                >
+                  Home
+                </div>
+              </li>
+              <li className="nav-item">
+                <div
+                  className="nav-link"
+                >
+                  Shopping Cart
+                </div>
+              </li>
+              <li className="nav-item dropdown">
+                <div
+                  className="nav-link dropdown-toggle"
+                  id="dropdown01"
+                >
+                  Username
+                </div>
+              </li>
+            </ul>
+            <form className="form-inline my-2 my-lg-0">
+              <input
+                className="form-control mr-sm-2"
+                type="text"
+                placeholder="Search"
+                aria-label="Search"
+              />
+              <button className="btn btn-secondary my-2 my-sm-0" type="submit">
+                Search
+              </button>
+            </form>
+          </div>
+        </nav>
     );
-}
+  };
 
 export default Header;
